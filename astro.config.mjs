a4c9e4b03b69df7ffa80614532d7b1a5bcd0ead2
Jason Moggridge <jason.moggridge@cdisoftware.com>
2022-10-18 14:12:02 -0400

    Fix astro configuration
    Remove the 'base' option from the astro config. I misread the
    documentation on it and it was breaking loading resources.

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,4 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://network-goods.github.io/arcological-website',
-    base: '/arcological-website',
 });
