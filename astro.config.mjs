17fb6bfa9b4e4a0e8e7be69160424c6507943d0f
Jason Moggridge <jason.moggridge@cdisoftware.com>
2022-10-18 15:47:40 -0400

    More silly misunderstandings of the docs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://network-goods.github.io/arcological-website',
+    site: 'https://network-goods.github.io',
+    base: '/arcological-website'
 });
