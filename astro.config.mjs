4a99b975e03bb150ff6bc7848e5bec2947023571
Addie Wagenknecht <addie@nortd.com>
2022-10-21 00:05:08 +0200

    Add files via upload

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,19 @@
 import { defineConfig } from 'astro/config';
+import { fileURLToPath } from 'url';
+import path, { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://network-goods.github.io',
-    base: '/arcological-website'
+    site: 'https://network-goods.github.io/',
+    base: '/arcological-website',
+    vite: {
+      resolve: {
+        alias: {
+          '@/': `${path.resolve(__dirname, 'src')}/`
+        }
+      }
+    }
 });
