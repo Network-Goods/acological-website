b7a4ec2ae14e3e63e0f5cc7e662f95c7b0011a41
Jason Moggridge <jason.moggridge@cdisoftware.com>
2022-10-21 10:19:13 -0400

    Fix problem from merging pull request
    This is what I get for trying to resolve a confict from the github
    UI, lesson learned.

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ const __dirname = dirname(__filename);
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://arcological.xyz',,
+    site: 'https://arcological.xyz',
     vite: {
       resolve: {
         alias: {
